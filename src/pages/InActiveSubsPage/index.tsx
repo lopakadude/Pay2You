697119dcc99eecedd88bd3a96893968edb42f995
Lopakadude <lopakadude@gmail.com>
2024-03-24 14:38:48 +0300

    fix: styles of headers

--- src/pages/InActiveSubsPage/index.tsx
+++ src/pages/InActiveSubsPage/index.tsx
@@ -8,7 +8,7 @@ export default function InActiveSubsPage() {
   return (
     <section className={styles.inActiveSubsPage}>
       <Navbar />
-      <h1 className={styles.activeSubsPage__header}>Неактивные подписки</h1>
+      <h1 className={styles.inActiveSubsPage__header}>Неактивные подписки</h1>
       <SubsList type="flex" colorSсheme="none-active" data={activeSubsArray}  />
     </section>
   );
