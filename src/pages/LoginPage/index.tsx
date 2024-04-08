c1886fa4bb203304c5b166bb5de2b258f7380dd2
Дмитрий Янюк <lopakadude@gmail.com>
2024-04-08 04:33:55 +0300

    Update index.tsx

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -10,7 +10,7 @@ export default function NotFoundPage() {
   const isLoggedIn = useAppSelector(
     (state: RootState) => state.auth.isLoggedIn
   );
-    const number = '1234567890';
+    const number = '555';
       const { setLoggedIn } = useActions();
       const [login] = useLoginMutation();
       const navigate = useNavigate(); 
